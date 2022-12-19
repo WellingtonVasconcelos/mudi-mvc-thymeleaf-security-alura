d9b5500dd6364fe095abe7e884f147938a828d77
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-12-19 11:43:03 -0300

    Working with pagination and enabling cache

--- src/main/java/br/com/wellington/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/wellington/mvc/mudi/repository/PedidoRepository.java
@@ -2,6 +2,8 @@ package br.com.wellington.mvc.mudi.repository;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,14 +14,16 @@ import br.com.wellington.mvc.mudi.model.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
-
-	List<Pedido> findByStatus(StatusPedido status);
+	
+	@Cacheable("pedidos")
+	List<Pedido> findByStatus  (StatusPedido status, Pageable sort);
 
 	@Query("select p from Pedido p join p.user u where u.username = :username")
 	List<Pedido> findAllByUser(@Param("username") String username);
 
 	@Query("select p from Pedido p join p.user u where u.username = :username and p.status = :status")
 	List<Pedido> findByStatusAndUser(@Param("status")StatusPedido status, @Param("username")String username);
-	
+
+
 
 }
